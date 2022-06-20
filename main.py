25e33a15aabef46791340dcb38d3a6b11e95115e
Katsutami7moto <nclsf87@gmail.com>
2022-06-20 17:43:39 +0300

    Download APOD pictures form NASA

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import os
+from datetime import datetime
 from pathlib import Path
-from urllib.parse import urlparse, unquote_plus
+from urllib.parse import urlparse
 
 import requests
-from datetime import datetime
+from dotenv import load_dotenv
+
+
+def fetch_nasa_apod_pics(api_key: str, count: int) -> list:
+    api_url = 'https://api.nasa.gov/planetary/apod'
+    params = {
+        'api_key': api_key,
+        'count': count,
+    }
+    api_response: dict = requests.get(url=api_url, params=params).json()
+    apods = filter(lambda x: 'hdurl' in x, api_response)
+    return [apod['hdurl'] for apod in apods]
 
 
 def fetch_spacex_last_launch() -> list:
@@ -15,35 +28,46 @@ def fetch_spacex_last_launch() -> list:
             return flickr_links
 
 
-def download_picture(pic_url: str, dir_name: str, file_name: str = ''):
+def get_file_extension_from_url(url: str) -> str:
+    return Path(urlparse(url).path).suffix
+
+
+def download_one_picture(pic_url: str, dir_name: str, file_name: str):
     pic_response = requests.get(pic_url)
     pic_response.raise_for_status()
     pics_dir = Path(dir_name)
     pics_dir.mkdir(parents=True, exist_ok=True)
-    parsed_url = urlparse(pic_url)
-    parsed_pic_name = Path(parsed_url.path).name
-    if file_name:
-        file_name += f".{parsed_pic_name.split('.')[-1]}"
-    else:
-        file_name = unquote_plus(parsed_pic_name)
+    file_name += get_file_extension_from_url(pic_url)
     file_path = pics_dir.joinpath(file_name)
     with open(file_path, 'wb') as file:
         file.write(pic_response.content)
 
 
-def main():
-    # Download a picture
+def download_pictures_to_dir(pics: list, prefix: str, dir_name: str):
+    now_formatted = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    for number, link in enumerate(pics):
+        file_name = f'{prefix}_{now_formatted}_{number:02d}'
+        download_one_picture(link, dir_name, file_name)
 
-    # url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
+def main():
+    load_dotenv()
+    nasa_api_key = os.getenv('NASA_API_KEY')
     dir_name = 'images'
 
     # Get pictures from SpaceX api
+    download_pictures_to_dir(
+        fetch_spacex_last_launch(),
+        'spacex',
+        dir_name
+    )
 
-    pic_links = fetch_spacex_last_launch()
-    now_formatted = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    for number, link in enumerate(pic_links):
-        file_name = f'spacex_{now_formatted}_{number:02d}'
-        download_picture(link, dir_name, file_name)
+    # Get APOD pictures from NASA api
+    download_pictures_to_dir(
+        fetch_nasa_apod_pics(nasa_api_key, 50),
+        'nasa_apod',
+        dir_name
+    )
 
 
 if __name__ == "__main__":
