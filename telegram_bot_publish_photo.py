cb37b2d7d6c5053c02145584b983079fa57d460e
Katsutami7moto <nclsf87@gmail.com>
2022-06-22 17:52:56 +0300

    Correct file opening

--- telegram_bot_publish_photo.py
+++ telegram_bot_publish_photo.py
@@ -11,7 +11,8 @@ def publish_photo(image_path: str):
     telegram_api_token: str = os.getenv('TELEGRAM_API_TOKEN')
     telegram_channel_id: str = os.getenv('TELEGRAM_CHANNEL_ID')
     bot = telegram.Bot(token=telegram_api_token)
-    bot.send_photo(
-        chat_id=telegram_channel_id,
-        photo=open(image_path, 'rb')
-    )
+    with open(image_path, 'rb') as image:
+        bot.send_photo(
+            chat_id=telegram_channel_id,
+            photo=image
+        )
